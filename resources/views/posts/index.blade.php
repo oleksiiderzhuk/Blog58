3c9087523d0edb64845beba365c98de2c79a43fb
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-12-10 15:42:55 +0200

    modify index blade for show method realization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,7 +27,7 @@
                         <div class="card-img"
                             style="background-image: url({{ $post->img ?? asset('img/0' . $i . '.jpg') }})"></div>
                         <div class="card-author">Автор: {{ $post->name }}</div>
-                        <a href="" class="btn btn-outline-primary">Посмотреть пост</a>
+                        <a href="{{ route('post.show', ['id' => $post->post_id]) }}" class="btn btn-outline-primary">Посмотреть пост</a>
                         <a href="#" class="btn btn-outline-primary">Button</a>
                         <a href="" class="btn btn-info">Info</a>
                     </div>
