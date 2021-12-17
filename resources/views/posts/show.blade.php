b4e4b3c8bc1ed586d2fc3ccbafe4c01508594a1f
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-12-17 10:38:33 +0200

    buttons for edit and destroy in show.blade, make routes...

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,7 +15,12 @@
                         style="background-image: url({{ asset('img/' . $post->img . '.jpg') }})"></div>
                     <div class="card-author">Автор: {{ $post->name }}</div>
                     <div class="card-date">Пост создан: {{ $post->created_at->diffForHumans() }}</div>
-                    <a href="{{ route('post.index') }}" class="btn btn-outline-primary">На главную</a>
+                    <div class="card-btn">
+                        <a href="{{ route('post.index') }}" class="btn btn-outline-primary">На главную</a>
+                        <a href="{{ route('post.edit', ['id'=>$post->post_id]) }}" class="btn btn-outline-success">Редактировать</a>
+                        <a href="{{ route('post.destroy', ['id'=>$post->post_id]) }}" class="btn btn-outline-danger">Удалить</a>
+
+                    </div>
                 </div>
             </div>
         </div>
