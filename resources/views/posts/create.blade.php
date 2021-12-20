3924d78fc5f6e42f991847d9ee735065cb608400
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-12-20 19:18:10 +0200

    dry dev

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,16 +5,7 @@
         @csrf
         <h3>Создать пост </h3>
         {{-- .form-group*3 --}}
-        <div class="form-group">
-            <input name="title" type="text" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <textarea name="descr" rows="10" class="form-control" required></textarea>
-        </div>
-        
-        <div class="form-group">
-            <input type="file" name="img" class="form-group">    
-        </div>
+        @include('posts.parts.form')
         
         <div class="form-group">
             <input type="submit" value="Создать пост" class="btn btn-outline-success">
