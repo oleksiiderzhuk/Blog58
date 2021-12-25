15df408410b44eb332260ffe8b8ba872f46f3199
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-12-25 15:42:21 +0200

    delete confirm with javascript

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,7 +19,7 @@
                     <div class="card-btn">
                         <a href="{{ route('post.index') }}" class="btn btn-outline-primary">На главную</a>
                         <a href="{{ route('post.edit', ['id'=>$post->post_id]) }}" class="btn btn-outline-success">Редактировать</a>
-                       <form action="{{ route('post.destroy', ['id'=>$post->post_id]) }}" method="post">
+                       <form action="{{ route('post.destroy', ['id'=>$post->post_id]) }}" method="post" onsubmit="if (confirm('Точно удалить пост?')){return true} else {return false}">
                             @csrf
                             @method('delete')
                             <input type="submit" class="btn btn-outline-danger" value="Удалить">
