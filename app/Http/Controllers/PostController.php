d61f3bacb79a492696d7391915e13fab83b8eeab
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-12-30 16:44:10 +0200

    save post author to the post and post edit buttons protection

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,7 +58,7 @@ class PostController extends Controller
         $post->title = $request->title;
         $post->short_title = Str::length($request->title)>30 ? Str::substr($request->title, 0, 30) . '...' : $request->title;
         $post->descr = $request->descr;
-        $post->author_id = rand(1, 4);
+        $post->author_id = \Auth::user()->id;
 
         if ($request->file('img')){
             $path = Storage::putFile('public', $request->file('img'));
