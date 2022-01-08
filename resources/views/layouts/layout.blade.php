c8b9559d28ec273121fa95729c2cc4530c59925c
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2022-01-08 17:49:36 +0200

    .env files prepared; new favicon added

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="shortcut icon" href="{{ asset('img/laravel_favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('img/laravel_icon.ico') }}">
     <title>{{ $title }}</title></title>
 </head>
 
